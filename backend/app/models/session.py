73ed2d6df1cd4d7cfe28b51a932875267fed9c71
Sruthiksha Ananth <sruthi@Sruthikshas-MacBook-Pro.local>
2026-06-02 16:29:55 +0100

    feat: add Session model with foreign keys to User and Student

---
+++ backend/app/models/session.py
@@ -0,0 +1,28 @@
+"""
+Represents a single tutoring session booked between tutor and student.
+Stores when a session is schedule, length, hourly rate and the status
+
+"""
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class Session(Base):
+    __tablename__= "sessions"
+
+    id = Column(Integer, primary_key = True, index = True)
+    tutor_id = Column(Integer, ForeignKey("user.id"), nullable = False) #foreign key - links back to User
+    student_id = Column(Integer, ForeignKey("students.id"), nullable=False) #foreign key - links back to Student
+    schedued_at = Column(DateTime(timezone=True), nullabe = False) #time required to book
+    duration_minutes = Column(Integer, default = 60) #default of 60 minutes
+    hourly_rate = Column(Float, default = 0.0)
+    status = Column(String, default = "scheduled") #tracks the lfecycle of the session - scheduled, completed and cancelled
+    notes = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default = func.now())
+
+    #define the relationships of the foreign keys
+    tutor = relationship("User", backref = "sessions")
+    student = relationship("Student", back_populates="sessions")
+    
+
---
+++ backend/app/models/student.py
@@ -0,0 +1,25 @@
+"""
+Model represents every student a tutor is teaching
+"""
+from sqlalchemy import Column, Integer, string, Text, ForeignKey, DateTime
+from sqlalchemy.orm import relationship #allows navigation across tables
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+#
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index = True)
+    tutor_id = Column(Integer, ForeignKey("users.id"), nullable=False) #linked to another table - connects students to tutors
+    full_name = Column(String, nullable = False)
+    email = Column(String)
+    subject = Column(String)
+    notes = Column(Text)
+    created_at = Column(DateTime(timezone=True), server_default = func.now())
+
+    #define any relationships with this table
+    sessions = relationship ("Session", back_populates = "student") #back_populates more explicit - can see the relationship defined on both files
+    tutor = relationship ("User", backref="students") #relationship - allows getting the tutors for each student
+    #backref - allows two way relationship of students to tutors and vice versa.
+
