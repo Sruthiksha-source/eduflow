055bff2babc14f4d8bdf28c710cce46d43764137
Sruthiksha <sruthikshaa07@gmail.com>
2026-08-22 17:41:28 +0100

    feat: add AuthContext with login, logout and persistent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider> {/*wraps the entire app*/}
+      {/* Contains all our routes and pages, sits insidde AuthProvider so all pages have access to global auth state*/}
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
